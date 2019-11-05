31dfb52748a0db110683fa0f749b0b55bcfe53a6
yangfan789789 <1062208122@qq.com>
2019-11-05 20:42:42 +0800

    yangfan

---
+++ build/precache-manifest.268ea937d8122cc77082e7f965dc914e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "58b75dd7d72a87ada892d3292a10390c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "27b457af32b1b5bb1a7b",
+    "url": "/static/css/2.802837ff.chunk.css"
+  },
+  {
+    "revision": "885a920a0a8dd2c47cf6",
+    "url": "/static/css/main.a22a4e25.chunk.css"
+  },
+  {
+    "revision": "27b457af32b1b5bb1a7b",
+    "url": "/static/js/2.acce8d71.chunk.js"
+  },
+  {
+    "revision": "885a920a0a8dd2c47cf6",
+    "url": "/static/js/main.81527563.chunk.js"
+  },
+  {
+    "revision": "83d97cf838c74a71b465",
+    "url": "/static/js/runtime-main.ab3ec558.js"
+  }
+]);
